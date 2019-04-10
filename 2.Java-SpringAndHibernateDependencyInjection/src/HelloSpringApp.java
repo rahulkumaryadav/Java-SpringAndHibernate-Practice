77bb2f02a0a6630dba6f042a2801d322762e47c5
rahulkumaryadav <rahulkumar96369@gmail.com>
2019-04-10 11:37:10 +0530

    commited DI for track coach

--- 2.Java-SpringAndHibernateDependencyInjection/src/HelloSpringApp.java
+++ 2.Java-SpringAndHibernateDependencyInjection/src/HelloSpringApp.java
@@ -4,10 +4,20 @@ public class HelloSpringApp {
 
 	public static void main(String[] args) {
 
+		
+		//load the spring configuration file
 		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("applicationConfig.xml");
 		
+		//retrieve bean from spring container
 		Coach theCoach=context.getBean("myCoach",Coach.class);
+		
+		//lets call the methods on bean
 		System.out.println(theCoach.getDailyFortune());
+		System.out.println(theCoach.getDailyWorkout());
+		
+		//close the context
+		context.close();
+
 	}
 
 }
--- 2.Java-SpringAndHibernateDependencyInjection/src/TrackCoach.java
+++ 2.Java-SpringAndHibernateDependencyInjection/src/TrackCoach.java
@@ -1,17 +1,24 @@
  
 
 public class TrackCoach implements Coach {
+	
+	//defining private field for dependency 
+	private FortuneService fortuneService;
+	
+
+	public TrackCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		// TODO Auto-generated method stub
-		return null;
+		return "Run a hard 5 k";
 	}
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Just do it"+fortuneService.getFortune();
 	}
 
 }
